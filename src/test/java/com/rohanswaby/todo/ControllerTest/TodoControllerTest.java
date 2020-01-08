9ba8492483198969fe26c29e45630cddf89e99c8
Rohan Swaby <rohan_swaby51@yahoo.com>
2020-01-07 19:09:38 -0500

    controllerTestCLass

--- src/test/java/com/rohanswaby/todo/ControllerTest/TodoControllerTest.java
+++ src/test/java/com/rohanswaby/todo/ControllerTest/TodoControllerTest.java
@@ -1,4 +1,17 @@
 package com.rohanswaby.todo.ControllerTest;
 
+import com.rohanswaby.todo.controller.TodoController;
+import com.rohanswaby.todo.service.TodoService;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.MockitoJUnitRunner;
+
+@RunWith(MockitoJUnitRunner.class)
 public class TodoControllerTest {
+    @InjectMocks
+    TodoController todoController;
+
+    @Mock
+    TodoService todoService;
 }
